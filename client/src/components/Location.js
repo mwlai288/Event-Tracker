f2d0e58180be55dede72775a43421b586444b0af
mwlai288 <mwlai288@gmail.com>
2017-08-11 18:24:34 -0400

    Populated location page

--- client/src/components/Location.js
+++ client/src/components/Location.js
@@ -1,40 +1,42 @@
-import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
+import React, {Component} from 'react';
+import {Link} from 'react-router-dom';
 import axios from 'axios';
 
 class Location extends Component {
 
-constructor (){
-    super();
-    this.state = {
-        location: ""
+    constructor() {
+        super();
+        this.state = {
+            location: []
+        }
+    }
+
+    componentWillMount() {
+
+        axios.get("/api/location").then((res) => {
+                console.log(res);
+                this.setState({location: res.data});
+                // id: res.data._id, location: res.data.location });
+            });
+    }
+
+    render() {
+        return (
+            <div>
+              <h1>Hello from Locations</h1>
+                <ul>
+                {this.state.location.map((location, i) => {
+                    return (
+                        <li key={i}>
+                           <Link to={`/location/${location._id}`}>
+                                {location.place}
+                           </Link>
+                        </li> );
+                        })
+                    } 
+                </ul>
+            </div>
+        )
     }
 }
-
-componentWillMount(){
-    axios.get('localhost:3001/api/location').then(res => {
-        this.setState({
-            id: res.data._id,
-            location: res.data.location,
-        });
-    });
-}
-
-
-  render() {
-    return (
-     <div>   
-      <h1>Hello from Locations</h1>
-      {this.props.location.map((location, i) => {
-          <div key={i}>
-              <Link to={`/location/${location._id}`}>
-                </Link>
-           </div>     
-      })}
-     </div>
-    );
-  }
-}
-
 export default Location;
-
