39bee6fe6b671c942483762153974af6f0dd12f8
mwlai288 <mwlai288@gmail.com>
2017-08-10 20:16:02 -0400

    Set up localhost3000

---
+++ server.js
@@ -0,0 +1,30 @@
+require("dotenv").config();
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const app = express();
+
+// mongoose.Promise = global.Promise;
+
+// Mongoose Connection
+mongoose.connect(process.env.MONGODB_URI);
+const connection = mongoose.connection;
+
+connection.on('connected', () => {
+  console.log('Mongoose Connected Successfully');    
+}); 
+
+connection.on('error', (err) => {  
+  console.log('Mongoose default connection error: ' + err);
+}); 
+
+app.use(bodyParser.json());
+
+app.get('/', (req,res) => {
+  res.send('HELLO DUDE!')
+})
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log("Stuff Happened on " + PORT);
+});
