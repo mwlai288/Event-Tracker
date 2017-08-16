ed53b5524856ba694bbc4390c3d2989115adef50
mwlai288 <mwlai288@gmail.com>
2017-08-15 22:18:13 -0400

    added background image and controllers for location put and delete

--- controllers/location.js
+++ controllers/location.js
@@ -26,4 +26,38 @@ router.post('/', (req,res) => {
   }).catch(err => console.log(err));
 });
 
+// Delete Location
+router.get('/:locationId/delete', (req,res) =>{
+  const locationId = req.params.locationId;
+  Location.findById(locationId).then((location)=>{
+    const foundUser = location.users.find((place) => {
+      return place.id === placeId
+    });
+    const foundLocation = foundUser.place.find((location) => {
+      return place.id === locationId;
+    });
+    foundUser.location.remove(foundUser);
+    location.save();
+    res.redirect(`/${locationId}`);
+  }).catch((error)=>{
+    console.log(error);
+  })
+});
+
+// Update Location
+router.put('/:location', (req, res) => {
+  const locationId = req.params.battleId;
+  console.log(locationId);
+  Location.findById(locationId).then((location) => {
+    const foundLocation = location.place.find((location) => {
+      return place.id === placeId;
+    });
+    foundLocation.place = req.body.place;
+    location.save();
+    console.log('SUCCESS');
+    });
+    }).catch((error) => {
+    console.log(error);
+    });
+    
 module.exports = router;
